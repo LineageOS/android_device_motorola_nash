b0d7bb27b5a17b1aceba3198a62510ef5235059b
Sooraj Sasindran <sasindran@google.com>
2017-07-07 10:10:57 -0700

    nash: Do not drop IMS packets
    Do not drop packets based upon enqueue sequence
    to avoid freeze.
    
    Bug=63274801
    
    Change-Id: I064dfc2775be104096566dff0c1638ac04cd7fc4

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -166,6 +166,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=10,0 \
     ro.use_data_netmgrd=true \
     telephony.lteOnCdmaDevice=1 \
+    persist.vendor.ims.dropset_feature=0 \
     persist.vendor.ims.disableDebugLogs=0 \
     persist.vendor.ims.disableIMSLogs=0 \
     persist.vendor.ims.disableDebugDataPathLogs=0 \
