478449984ce3f653c6b759fed3cb21d75f5c3464
Dan Pasanen <dan.pasanen@gmail.com>
2018-01-13 11:26:46 -0600

    nash: fix fstab location

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,7 +9,6 @@ LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_VENDOR_MODULE    := true
-LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
