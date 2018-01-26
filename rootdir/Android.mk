d714b287feccb068cec152b3e352d3a00e6d7457
Dan Pasanen <dan.pasanen@gmail.com>
2018-01-25 22:37:18 -0600

    nash: add init.gbmods.sh
    Change-Id: I2985b1ea82d3fbdd4a0c7f5b26b523ed00159bd3

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -12,6 +12,14 @@ LOCAL_VENDOR_MODULE    := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.gbmods.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := bin/init.gbmods.sh
+LOCAL_VENDOR_MODULE    := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mmi.charge_only.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
---
+++ rootdir/bin/init.gbmods.sh
@@ -0,0 +1,30 @@
+#!/vendor/bin/sh
+
+debug=$(getprop ro.boot.gbdebug 2> /dev/null)
+bootmode=$(getprop ro.bootmode 2> /dev/null)
+
+# If androidboot.gbdebug is set on command line, skip inserting
+# the pre-installed modules.
+if [ "$debug" == "1" ]; then
+    return 0
+fi
+
+insmod /vendor/lib/modules/greybus.ko
+
+# Only support PTP and BATTERY in charge-only mode
+if [ "$bootmode" == "charger" ]; then
+    insmod /vendor/lib/modules/gb-mods.ko
+    insmod /vendor/lib/modules/gb-battery.ko
+    insmod /vendor/lib/modules/gb-ptp.ko
+
+    return 0
+fi
+
+gbmods="/vendor/lib/modules/gb-*"
+for mod in $gbmods
+do
+    insmod $mod
+done
+
+insmod /vendor/lib/modules/v4l2-hal.ko
+start mods_camd
