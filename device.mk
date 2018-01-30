b0d337db821e7e88e7e718531599c065ec21f9d5
Dan Pasanen <dan.pasanen@gmail.com>
2018-01-29 21:00:19 -0600

    nash: add daemon, init script and permissions for laser focus sensor
    * Also sneak in missing libSonyDefocus.so
    
    Change-Id: I5084b8487f16cb68ed07caf0dc8335024605572d

--- device.mk
+++ device.mk
@@ -187,6 +187,7 @@ PRODUCT_PACKAGES += \
     init.mmi.diag_mdlog.rc \
     init.mmi.hab.rc \
     init.mmi.hab.sh \
+    init.mmi.laser.sh \
     init.mmi.modprobe.rc \
     init.mmi.overlay.rc \
     init.mmi.ramdump.rc \
