b6f36474edc8e39dc376adbf9dc6a1ffdf366244
Dan Pasanen <dan.pasanen@gmail.com>
2018-02-10 21:35:02 -0600

    nash: remove now non-existent bootctrl hal override flag
    Change-Id: Ibf363475a8ebadf92301f322b4a23c1d0146daa5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,9 +98,6 @@ BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
 QCOM_BT_USE_BTNV := true
 
-# Bootctrl HAL
-BOARD_PROVIDES_BOOTCTRL_HAL := true
-
 # Camera
 USE_CAMERA_STUB := true
 
