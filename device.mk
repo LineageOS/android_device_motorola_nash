32ec27555a1f9bfd6dad340d339b8013115d434e
Nolen Johnson <johnsonnolen@gmail.com>
2018-06-18 10:46:49 -0400

    nash: Exclude ModService from battery optimization
    Change-Id: I7a9f1a1f99144cd193cbb372773391de20b7dda1

--- device.mk
+++ device.mk
@@ -265,6 +265,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# ModService
+PRODUCT_COPY_FILES += \
+    device/motorola/nash/whitelist_modservice.xml:system/etc/sysconfig/whitelist_modservice.xml
+
 # Network
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
