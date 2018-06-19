7cd60465dd14b0c38d86f47c63c0bb26bf66159f
Erfan Abdi <erfangplus@gmail.com>
2018-06-19 09:47:50 +0430

    nash: add media_profiles and media_profiles_vendor
    Change-Id: Ia1911a2ada8f9148bd161729237c15e6f6d84376
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -259,6 +259,9 @@ PRODUCT_BOOT_JARS += \
 # Media
 PRODUCT_COPY_FILES += \
     device/motorola/nash/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
+    device/motorola/nash/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
+    device/motorola/nash/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
+    device/motorola/nash/media/media_profiles_vendor.xml:system/etc/media_profiles.xml \
     device/motorola/nash/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     device/motorola/nash/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
