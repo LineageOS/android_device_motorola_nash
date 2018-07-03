xiaomi-sdm660/android_device_xiaomi_sdm660-common

Set interval to large value for single mode
For RECURRENCE_SINGLE mode, set interval to a very large value:
std::numeric_limits<>::max()

Change-Id: Id9f2ddd1948d9660f69a308b3fe6ad6e6dcc6a77
CRs-Fixed: 2260711

android/location_api/GnssAPIClient.cpp
@@ -166,6 +166,10 @@ bool GnssAPIClient::gnssSetPositionMode(IGnss::GnssPositionMode mode,
     memset(&mTrackingOptions, 0, sizeof(TrackingOptions));
     mTrackingOptions.size = sizeof(TrackingOptions);
     mTrackingOptions.minInterval = minIntervalMs;
+    if (IGnss::GnssPositionRecurrence::RECURRENCE_SINGLE == recurrence) {
+        mTrackingOptions.minInterval =
+                std::numeric_limits<decltype(mTrackingOptions.minInterval)>::max();
+    }
     mTrackingOptions.minDistance = preferredAccuracyMeters;
     if (mode == IGnss::GnssPositionMode::STANDALONE)
         mTrackingOptions.mode = GNSS_SUPL_MODE_STANDALONE;

LineageOS/android_device_motorola_nash

android/location_api/GnssAPIClient.cpp
@@ -165,6 +165,10 @@ bool GnssAPIClient::gnssSetPositionMode(IGnss::GnssPositionMode mode,
     mLocationOptions.size = sizeof(LocationOptions);
     mLocationOptions.minInterval = minIntervalMs;
     mLocationOptions.minDistance = preferredAccuracyMeters;
+    if (IGnss::GnssPositionRecurrence::RECURRENCE_SINGLE == recurrence) {
+        mLocationOptions.minInterval =
+                std::numeric_limits<decltype(mLocationOptions.minInterval)>::max();
+    }
     if (mode == IGnss::GnssPositionMode::STANDALONE)
         mLocationOptions.mode = GNSS_SUPL_MODE_STANDALONE;
     else if (mode == IGnss::GnssPositionMode::MS_BASED)