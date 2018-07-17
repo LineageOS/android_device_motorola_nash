85bbe916366b8d42f79bbb9c57fdc13b6760f514
Erfan Abdi <erfangplus@gmail.com>
2018-07-17 12:29:24 +0430

    nash: Convert libinit to init scripts
    * Also set ro.product.manufacturer to Motorola for camera HAL in GSIs.
    
    Change-Id: Ica65984fa06a661b831c151593e3ee92c596009d
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -89,6 +89,15 @@ LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.nash.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.nash.rc
+LOCAL_VENDOR_MODULE    := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.sensors.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := EXECUTABLES
