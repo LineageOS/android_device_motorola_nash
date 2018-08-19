dda187ecb96e3b0987e02ffab5a7e1cfbb608ad6
Nolen Johnson <johnsonnolen@gmail.com>
2018-08-19 22:29:35 +0200

    [DNM] Revert "nash: Set SE Linux to Enforcing"
    This reverts commit 190e8347726b58107d7731e9a2e474c837d9e2a0.
    
    Change-Id: I52f4aff4e1eff5d1a724bb6d70e3f5bd348314d5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,7 +149,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048 androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3 firmware_class.path=/vendor/firmware_mnt/image
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
