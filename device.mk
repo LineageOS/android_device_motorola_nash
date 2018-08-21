f363787b25c16aa66e041686463b5c47eada0f92
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-20 19:59:05 -0700

    nash: "borrow" health@2.0 HAL from wahoo
    Change-Id: I68d772e43400c285dc4dd05a43c03026fd1e25d5

--- device.mk
+++ device.mk
@@ -156,8 +156,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-service.mata
 
 # HIDL
 PRODUCT_PACKAGES += \
