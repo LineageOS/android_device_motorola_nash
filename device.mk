1442c4394ccb9366b7715e1bff15649d38ab24d2
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-21 22:42:45 -0700

    nash: Move to the new 9.0 Audio IMPL 4.0
    Change-Id: Idfdb881ae71431fc47e7df55e467cf549b7090e6
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -24,9 +24,9 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.0-impl \
     android.hardware.soundtrigger@2.0-service \
