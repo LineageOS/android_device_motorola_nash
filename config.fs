859aab57bb49258a0b1ea6b874945002406207a4
Erfan Abdi <erfangplus@gmail.com>
2018-08-30 20:07:04 +0300

    nash: Update fstab.qcom from 9.0
    * Also cleanup rootfs.
    
    Change-Id: I09079a5bb16fa5226909b9561dcb7dce4de4fb3e
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- config.fs
+++ config.fs
@@ -88,26 +88,8 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE BLOCK_SUSPEND
 
-[firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[bt_firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
 [persist/]
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: 0
-
-[dsp/]
-mode: 0771
-user: AID_MEDIA
-group: AID_MEDIA
 caps: 0
