2713d130676e59d6c311b29c0f6a23051e41d4e4
Erfan Abdi <erfangplus@gmail.com>
2018-09-01 20:09:03 +0300

    nash: Cleanup /persist and /fsg in rootdir
    * These are now mounted on /vendor.
    * Also cleans the up.
    
    Change-Id: I17416ea87834c56c73b25186e723b6595075c0e5
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- config.fs
+++ config.fs
@@ -88,8 +88,38 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE BLOCK_SUSPEND
 
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[vendor/firmware_mnt/image/*]
+mode: 0771
+user: AID_ROOT
+group: AID_SYSTEM
+caps: 0
+
+[bt_firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
 [persist/]
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
+caps: 0
+
+[dsp/]
+mode: 0771
+user: AID_MEDIA
+group: AID_MEDIA
 caps: 0
