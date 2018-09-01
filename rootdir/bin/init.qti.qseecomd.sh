67a3949ac86b8070494a2be7ce6b9137a1ef8135
Erfan Abdi <erfangplus@gmail.com>
2018-09-01 20:45:12 +0300

    nash: init-files: Update for 9.0
    * TODO: Explain this.
    
    Change-Id: I2c52646548f198cdbf79015ffc81d0e743fb75c6
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- rootdir/bin/init.qti.qseecomd.sh
+++ rootdir/bin/init.qti.qseecomd.sh
@@ -1,5 +1,5 @@
 #!/vendor/bin/sh
-# Copyright (c) 2016,2017 The Linux Foundation. All rights reserved.
+# Copyright (c) 2016, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -31,5 +31,5 @@
 while [ "$registered" != "true" ]
 do
     sleep 0.1
-    registered="`getprop sys.listeners.registered`"
+    registered="`getprop vendor.sys.listeners.registered`"
 done
