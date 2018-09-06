416c1658c1dc9ed5ec16ba8b0c00b03ec7beb14d
Bruno Martins <bgcngm@gmail.com>
2018-09-06 05:43:49 +0000

    nash: Switch to common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- device.mk
+++ device.mk
@@ -398,7 +398,7 @@ PRODUCT_PACKAGES += brillo_update_payload
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.basic
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/1da4000.ufshc/by-name/system
