0f37ee8eaa480d8500000b792af831224efcc33c
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    nash: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- device.mk
+++ device.mk
@@ -20,8 +20,7 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # A/B updater
 AB_OTA_PARTITIONS += \
