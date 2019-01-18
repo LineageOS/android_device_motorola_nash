d61e69eb9bab1ea56b3b23b3355f547e4f06fe84
Anton Hansson <hansson@google.com>
2019-01-18 14:10:45 +0000

    nash: Use TARGET_SCREEN_DENSITY
    This variable is used to auto-populate the ro.sf.lcd_density as
    well as selecting density bucket for resources in recovery.
    
    Bug: 118676832
    Test: adb shell getprop ro.sf.lcd_density
    Test: boot into recovery
    Change-Id: Ib383d18b61e7a2058a69ae26136aa053fdba4df2

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -73,8 +73,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1 \
     dev.pm.dyn_samplingrate=1 \
     ro.opengles.version=196610 \
-    ro.qualcomm.cabl=0 \
-    ro.sf.lcd_density=560
+    ro.qualcomm.cabl=0
 
 # Factory reset partition
 PRODUCT_PROPERTY_OVERRIDES += \
