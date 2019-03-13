94cdc391c6c680519beaf7ae5cf4aff27438b283
Pawin Vongmasa <pawin@google.com>
2019-03-12 19:56:00 -0700

    nash: Enable Codec2
    Drop framework_manifest.xml part as the hal is already parsed.
    Don't bring in the c2 xml files as CAF squashed the c2
    support in the already existing media codecs.
    
    Change-Id: Ibe051f4b19c2dbb0a208e751a3b7a332a329b447
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -63,6 +63,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.max.previewfps=60 \
     vidc.enc.dcvs.extra-buff-count=2
 
+# Codec2 switch
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.media.codec2=2
+
 # Color Mode
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_default_color_mode=1
