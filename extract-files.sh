02f90307551912610d355682da3f434357496381
Erfan Abdi <erfangplus@gmail.com>
2019-06-07 05:05:14 +0300

    nash: update blobs and configs from PPX29.159-10
    * fingerprint: motorola/nash/nash:9/PPX29.159-10/7f200:user/release-keys
    
    Change-Id: I4532de828d474a6078d5db4f0e6727a7bc04a7cf
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -59,6 +59,18 @@ extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
 BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
+# Load libSonyDefocus from vendor
+CAMERA_IMX386="$BLOB_ROOT"/vendor/lib/libmmcamera_imx386.so
+sed -i "s|/system/lib/hw/|/vendor/lib/hw/|g" "$CAMERA_IMX386"
+
+# Load ZAF configs from vendor
+ZAF_CORE="$BLOB_ROOT"/vendor/lib/libzaf_core.so
+sed -i "s|/system/etc/zaf|/vendor/etc/zaf|g" "$ZAF_CORE"
+
+# Load camera configs from vendor
+CAMERA2_SENSOR_MODULES="$BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
+sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$CAMERA2_SENSOR_MODULES"
+
 # Load wrapped shim
 MDMCUTBACK="$BLOB_ROOT"/vendor/lib64/libmdmcutback.so
 sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "$MDMCUTBACK"
