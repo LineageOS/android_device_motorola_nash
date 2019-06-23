27d53e377c9a6356dfe660038cb0375190cdfe9c
Erfan Abdi <erfangplus@gmail.com>
2019-06-23 14:34:32 +0300

    nash: build sysfs livedisplay
    Change-Id: Ie3d400c4f6950d5d91bd625be0e0d20523f2de0d
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -241,7 +241,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # Media
 PRODUCT_COPY_FILES += \
