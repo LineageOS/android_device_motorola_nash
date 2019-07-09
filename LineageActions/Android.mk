7cb2757a4e5c05f8a0a55bd6db13bf8396ea3e08
Bruno Martins <bgcngm@gmail.com>
2019-07-09 22:31:00 +0100

    nash: LineageActions: Migrate to androidx
    Change-Id: Ifaa67283a3437549a2304088a0ce377b399da19f
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- LineageActions/Android.mk
+++ LineageActions/Android.mk
@@ -12,10 +12,8 @@ LOCAL_PROPRIETARY_MODULE := true
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v7-appcompat \
-    android-support-v7-preference \
-    android-support-v7-recyclerview \
-    android-support-v14-preference
+    androidx.core_core \
+    androidx.preference_preference
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
@@ -31,5 +29,3 @@ endif
 include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
