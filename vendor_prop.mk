9360c670f719f1334223955da83b37049ecbddb1
Sujin Panicker <spanic@codeaurora.org>
2019-09-09 10:55:38 +0530

    nash: Add 48khz sampling rate to spkr prot tx uc
    With 8Khz sampling rate afe fails to fetch calibration
    information.
    sdm660 & msm8998 acdb supports only 48Khz for speaker
    protection calibration tx device, since 8Khz tables were
    not added initially, addition of 8Khz would require
    further tuning.
    This change is to use sample rate based on property, the
    default sample rate is not changed for other targets without
    prop.
    
    Change-Id: I29dc549c699396fc953f3f831cd0ea81ee033ae4

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -40,6 +40,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.offload.track.enable=true \
     vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.spkr_prot.tx.sampling_rate=48000 \
     vendor.audio.tunnel.encode=false \
     vendor.audio.use.sw.alac.decoder=false \
     vendor.audio.use.sw.ape.decoder=false \
