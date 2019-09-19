f991e29e62fc9982c5c7616b57a9371908af90af
Erfan Abdi <erfangplus@gmail.com>
2019-09-20 01:48:13 +0430

    nash: Convert java KeyDisabler to Touch HAL
    Change-Id: I19d4736a01604b354e43029ea77e54b698517954

--- device.mk
+++ device.mk
@@ -242,6 +242,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageActions
 
+# Lineage hardware
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.nash
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
