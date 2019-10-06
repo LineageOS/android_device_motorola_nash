cde1597b29b3bc47524c74a36cd00083ee6c3808
Pranav Vashi <neobuddy89@gmail.com>
2019-10-06 11:32:22 +0530

    nash: Switch to 1.2 version of qcom power HAL
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I386f42425e612eef7a14310bb4ca07a54efad272

--- device.mk
+++ device.mk
@@ -352,7 +352,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # QCOM
 PRODUCT_COPY_FILES += \
