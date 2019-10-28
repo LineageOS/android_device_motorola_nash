89eb349b49382d4bb40d8dc6bc2a3df34f3a51c3
Marc Bourgoin <themard85@gmail.com>
2019-10-28 00:04:59 -0700

    nash: update blob fixup for new extractor templates
    * pin  vendor/lib/libmot_gpu_mapper.so  to  prevent  adding  the
      same dependency multiple times if vendor blobs are regenerated.
    
    Change-Id: Ie8bcdd8f2fa4b277c7acbb6d9d5788084c2bbab8

--- extract-files.sh
+++ extract-files.sh
@@ -59,59 +59,76 @@ if [ -z "${SRC}" ]; then
     SRC=adb
 fi
 
+function blob_fixup() {
+    case "${1}" in
+
+    # Load libSonyDefocus from vendor
+    vendor/lib/libmmcamera_imx386.so)
+        sed -i "s|/system/lib/hw/|/vendor/lib/hw/|g" "${2}"
+        ;;
+
+    # Load ZAF configs from vendor
+    vendor/lib/libzaf_core.so)
+        sed -i "s|/system/etc/zaf|/vendor/etc/zaf|g" "${2}"
+        ;;
+
+    # Load camera configs from vendor
+    vendor/lib/libmmcamera2_sensor_modules.so)
+        sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "${2}"
+        ;;
+
+    # Drod unused dependency
+    vendor/lib/libmmcamera_vstab_module.so)
+        patchelf --remove-needed libandroid.so "${2}"
+        ;;
+
+    # Load camera metadata shim
+    vendor/lib/hw/camera.msm8998.so)
+        patchelf --replace-needed libcamera_client.so libcamera_metadata_helper.so "${2}"
+        ;;
+
+    # Load wrapped shim
+    vendor/lib64/libmdmcutback.so)
+        sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "${2}"
+        ;;
+
+    # Correct mods gid
+    etc/permissions/com.motorola.mod.xml)
+        sed -i "s|vendor_mod|oem_5020|g" "${2}"
+        ;;
+
+    # Correct qcrilhook library location
+    vendor/etc/permissions/qcrilhook.xml)
+        sed -i "s|/system/framework/qcrilhook.jar|/vendor/framework/qcrilhook.jar|g" "${2}"
+        ;;
+
+    # Correct QtiTelephonyServicelibrary location
+    vendor/etc/permissions/telephonyservice.xml)
+        sed -i "s|/system/framework/QtiTelephonyServicelibrary.jar|/vendor/framework/QtiTelephonyServicelibrary.jar|g" "${2}"
+        ;;
+
+    # Correct android.hidl.manager@1.0-java jar name
+    vendor/etc/permissions/qti_libpermissions.xml)
+        sed -i "s|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g" "${2}"
+        ;;
+
+    # Add uhid group for fingerprint service
+    vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc)
+        sed -i "s/system input/system uhid input/" "${2}"
+        ;;
+
+    # Load libmot_gpu_mapper shim (file is pinned to prevent adding the same dependency multiple times if vendor blobs are regenerated)
+    # vendor/lib/libmot_gpu_mapper.so)
+        # patchelf --add-needed libgpu_mapper_shim.so "${2}"
+        # ;;
+
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
 
-BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
-
-# Load libSonyDefocus from vendor
-CAMERA_IMX386="$BLOB_ROOT"/vendor/lib/libmmcamera_imx386.so
-sed -i "s|/system/lib/hw/|/vendor/lib/hw/|g" "$CAMERA_IMX386"
-
-# Load ZAF configs from vendor
-ZAF_CORE="$BLOB_ROOT"/vendor/lib/libzaf_core.so
-sed -i "s|/system/etc/zaf|/vendor/etc/zaf|g" "$ZAF_CORE"
-
-# Load camera configs from vendor
-CAMERA2_SENSOR_MODULES="$BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
-sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$CAMERA2_SENSOR_MODULES"
-
-# Drod unused dependency
-CAMERA_VSTAB_MODULES="$BLOB_ROOT"/vendor/lib/libmmcamera_vstab_module.so
-patchelf --remove-needed libandroid.so "$CAMERA_VSTAB_MODULES"
-
-# Load camera metadata shim
-CAMERAHAL="$BLOB_ROOT"/vendor/lib/hw/camera.msm8998.so
-patchelf --replace-needed libcamera_client.so libcamera_metadata_helper.so "$CAMERAHAL"
-
-# Load wrapped shim
-MDMCUTBACK="$BLOB_ROOT"/vendor/lib64/libmdmcutback.so
-sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "$MDMCUTBACK"
-
-# Correct mods gid
-MODPERM="$BLOB_ROOT"/etc/permissions/com.motorola.mod.xml
-sed -i "s|vendor_mod|oem_5020|g" "$MODPERM"
-
-# Correct qcrilhook library location
-QCRILHOOK="$BLOB_ROOT"/vendor/etc/permissions/qcrilhook.xml
-sed -i "s|/system/framework/qcrilhook.jar|/vendor/framework/qcrilhook.jar|g" "$QCRILHOOK"
-
-# Correct QtiTelephonyServicelibrary location
-TELESERVICELIB="$BLOB_ROOT"/vendor/etc/permissions/telephonyservice.xml
-sed -i "s|/system/framework/QtiTelephonyServicelibrary.jar|/vendor/framework/QtiTelephonyServicelibrary.jar|g" "$TELESERVICELIB"
-
-# Correct android.hidl.manager@1.0-java jar name
-QTI_LIBPERMISSIONS="$BLOB_ROOT"/vendor/etc/permissions/qti_libpermissions.xml
-sed -i "s|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g" "$QTI_LIBPERMISSIONS"
-
-# Add uhid group for fingerprint service
-FP_SERVICE_RC="$BLOB_ROOT"/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc
-sed -i "s/input/uhid input/" "$FP_SERVICE_RC"
-
-# Load libmot_gpu_mapper shim
-MOT_GPU_MAPPER="$BLOB_ROOT"/vendor/lib/libmot_gpu_mapper.so
-patchelf --add-needed libgpu_mapper_shim.so "$MOT_GPU_MAPPER"
 
 "${MY_DIR}/setup-makefiles.sh"
