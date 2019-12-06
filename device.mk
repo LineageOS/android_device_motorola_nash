0be7b6c30a2232fa622b07c2fce51b7aa8a88ca3
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 13:58:23 -0800

    nash: Use 4GB dalvik-heap config
    We are currently using 2GB dalvik-heap config.
    
    Bug: 145823510
    Test: MPTS test
    Change-Id: Ie26b8e4fefea25fa818bd1eaf9df4a0bb153d42d

--- device.mk
+++ device.mk
@@ -126,13 +126,7 @@ PRODUCT_PACKAGES += \
     libsfplugin_ccodec
 
 # Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Dex-pre-opt exclusions
 $(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
