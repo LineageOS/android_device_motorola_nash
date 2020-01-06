84183baa906961328385efe6b704f857c6e5c291
Davide Garberi <dade.garberi@gmail.com>
2020-01-06 15:27:10 +0100

    nash: Camera: Add Sepolicy for accessing vendor properties
    * Reference: https://github.com/LineageOS/android_device_qcom_sepolicy/commit/c7c6752901641fbb3605e61aa0844b286cebe2d8
    
    Change-Id: I8f20d0066641c40f209369b47104397b84770aba

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,6 +147,7 @@ VENDOR_SECURITY_PATCH := 2019-11-01
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-mods/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy-mods/private
 
