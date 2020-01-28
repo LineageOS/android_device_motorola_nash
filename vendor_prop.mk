648c25baabc502832608f29bee584f3c0f6fa14f
Erfan Abdi <erfangplus@gmail.com>
2020-01-28 16:16:17 +0330

    nash: update BT a2dp offload prop
    This reverts commit 97ffd0856f6dfaab72d95a9817948da0eb8e0ffe.
    
    Change-Id: If514b0c325848f3a043338567795063408a1a485

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -92,7 +92,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bt.max.hfpclient.connections=1 \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
     vendor.qcom.bluetooth.soc=cherokee \
     ro.bluetooth.a4wp=false
 
