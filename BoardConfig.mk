386772c4b3a0ef6bad6eda2a2c2561f4ede543be
Erfan Abdi <erfangplus@gmail.com>
2020-01-29 13:19:16 +0330

    nash: Set verity to EIO mode
    Change-Id: I64f12657ce0d0b7a8e364c5e5b1da26177dac184

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,6 +96,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=2048 androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 sched_enable_power_aware=1
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.veritymode=eio
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
