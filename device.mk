8003832445c412284127f9eb72cc1dfe9b2d41f8
Erfan Abdi <erfangplus@gmail.com>
2020-07-28 00:05:09 +0430

    nash: Add PocketMode app
    * Based off android_device_xiaomi_jason
    
    Change-Id: I6dcb2e767be649094fbcc21f0aab244a40d48b9e

--- device.mk
+++ device.mk
@@ -353,6 +353,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
+# PocketMode
+PRODUCT_PACKAGES += \
+    MotoPocketMode
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
