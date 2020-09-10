e132cea0de07f0c1f25c9c2bd5b1a13b3d5cb900
Davide Garberi <dade.garberi@gmail.com>
2020-09-10 03:57:15 +0200

    nash: Build libhidltransport and libhwbinder
    Change-Id: I1b4b255ce40c94e3bf2e6234b7280fc094a9270b

--- device.mk
+++ device.mk
@@ -206,7 +206,11 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0-java \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # IMS
 PRODUCT_PACKAGES += \
