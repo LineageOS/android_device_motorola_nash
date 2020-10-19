99c68f0060862c27722009b2c736596ae04385cd
LuK1337 <priv.luk@gmail.com>
2020-10-19 21:42:02 +0200

    nash: TetheringOverlay: Disable BPF
    * Such is life with 4.4 kernels.
    
    Change-Id: I1c5e3fab63271b1e3fe6c814d96edbde782f27e8

--- device.mk
+++ device.mk
@@ -468,6 +468,9 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
 
