5268b2d1d543aeb27180b344c99f51f80c2f29ae
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    nash: Switch to standalone extract utils
    * Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- extract-files.sh
+++ extract-files.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -25,9 +15,9 @@ VENDOR=motorola
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -36,8 +26,9 @@ source "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
-SECTION=
+
 KANG=
+SECTION=
 
 while [ "$1" != "" ]; do
     case "$1" in
@@ -61,28 +52,23 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-
-    # Load wrapped shim
-    vendor/lib64/libmdmcutback.so)
-        sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "${2}"
-        ;;
-
-    # Fix missing symbols
-    vendor/lib64/libril-qc-hal-qmi.so)
-        patchelf --add-needed "libcutils_shim.so" "${2}"
-        ;;
-
-    # Fix xml version
-    product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
-        sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
-        ;;
-
+        # Fix xml version
+        product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
+            sed -i 's|xml version="2.0"|xml version="1.0"|g' "${2}"
+            ;;
+        # Load wrapped shim
+        vendor/lib64/libmdmcutback.so)
+            sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "${2}"
+            ;;
+        # Fix missing symbols
+        vendor/lib64/libril-qc-hal-qmi.so)
+            "${PATCHELF}" --add-needed "libcutils_shim.so" "${2}"
+            ;;
     esac
 }
 
-
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
 
