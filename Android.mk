492e18d78266a6ee7879d230d7300119ddf9bb42
Nolen Johnson <johnsonnolen@gmail.com>
2021-02-01 15:49:18 -0500

    nash: Remove legacy msadsp symlink
    * No longer needed.
    
    Change-Id: Ic557b4ac51ed370bfbc29289b99dd9554787f9ae

--- Android.mk
+++ Android.mk
@@ -31,10 +31,6 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-$(shell mkdir -p $(TARGET_OUT_VENDOR)/firmware; \
-    ln -sf /dev/block/bootdevice/by-name/msadp \
-        $(TARGET_OUT_VENDOR)/firmware/msadp)
-
 #A/B builds require us to create the mount points at compile time.
 #Just creating it for all cases since it does not hurt.
 FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
