176ff554f5b520be3113d345d0a5e9d13af3cd2f
Jeferson <jroliveira.oliveira301@gmail.com>
2021-04-17 21:02:09 +0200

    nash: Downrev audio to 4.0
    * Audio v5 has issues related with setMicrophoneFieldDimension, killing audio input causing
      signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0x0
    
    Change-Id: I9ce36ef6e9bffffdf32145372f6245892c4c1dd2

--- device.mk
+++ device.mk
@@ -35,9 +35,9 @@ PRODUCT_AAPT_PREF_CONFIG := 560dpi
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio.service \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.r_submix.default \
