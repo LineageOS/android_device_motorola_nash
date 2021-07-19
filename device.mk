e502745a528b08392e8cdbfed79d71e022a83163
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-19 16:07:34 +0200

    nash: Downrev soundtrigger to 2.1
    * Soundtrigger HAL doesn't really like new HIDL versions
    
    Change-Id: I5503753d4ba35a81e0e84963faeb1deaf1ee2223

--- device.mk
+++ device.mk
@@ -38,7 +38,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
