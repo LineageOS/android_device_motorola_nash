a86662964b5f5d108fe046d267161ded1114b9ca
Erfan Abdi <erfangplus@gmail.com>
2021-10-20 17:20:19 +0300

    nash: libshims: Add shim for camera blobs
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I5e622e68422f5c4da13c0752a7eb3f4b20ddae49

--- extract-files_nash.sh
+++ extract-files_nash.sh
@@ -52,6 +52,12 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        # Fix missing symbols
+        product/lib64/lib-imscamera.so | product/lib64/lib-imsvideocodec.so | product/lib/lib-imscamera.so | product/lib/lib-imsvideocodec.so)
+            for LIBGUI_SHIM in $(grep -L "libgui_shim.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"
+            done
+            ;;
         # Load libSonyDefocus from vendor
         vendor/lib/libmmcamera_imx386.so)
             sed -i "s|/system/lib/hw/|/vendor/lib/hw/|g" "${2}"
@@ -67,6 +73,12 @@ function blob_fixup() {
         # Drod unused dependency
         vendor/lib/libmmcamera_vstab_module.so)
             "${PATCHELF}" --remove-needed libandroid.so "${2}"
+            ;;
+        # Fix missing symbols
+        vendor/lib/libmot_gpu_mapper.so)
+            for LIBGUI_SHIM in $(grep -L "libgui_shim_vendor.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libgui_shim_vendor.so" "${LIBGUI_SHIM}"
+            done
             ;;
         # Load camera metadata shim
         vendor/lib/hw/camera.msm8998.so)
