1293caddf0fc90c8d57438b48c2535b08a85eeaa
Georg Veichtlbauer <georg@vware.at>
2023-04-15 21:57:57 +0200

    nash: Initial integration with msm8998-common
    Change-Id: I0e4f04c0b1f9aa816443b75f65cdb2485f01b512

--- extract-files.sh
+++ extract-files.sh
@@ -52,25 +52,49 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        # Fix xml version
-        product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
-            sed -i 's|xml version="2.0"|xml version="1.0"|g' "${2}"
-            ;;
-        # memset shim
-        vendor/bin/charge_only_mode)
-            for  LIBMEMSET_SHIM in $(grep -L "libmemset_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libmemset_shim.so" "$LIBMEMSET_SHIM"
+        # Fix missing symbols
+        product/lib64/lib-imscamera.so | product/lib64/lib-imsvideocodec.so | product/lib/lib-imscamera.so | product/lib/lib-imsvideocodec.so)
+            for LIBGUI_SHIM in $(grep -L "libgui_shim.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"
             done
             ;;
-        # Load wrapped shim
-        vendor/lib64/libmdmcutback.so)
-             "${PATCHELF}" --replace-needed "libqsap_sdk.so" "libqsap_shim.so" "${2}"
+        # Load libSonyDefocus from vendor
+        vendor/lib/libmmcamera_imx386.so)
+            sed -i "s|/system/lib/hw/|/vendor/lib/hw/|g" "${2}"
+            ;;
+        # Load ZAF configs from vendor
+        vendor/lib/libzaf_core.so)
+            sed -i "s|/system/etc/zaf|/vendor/etc/zaf|g" "${2}"
+            ;;
+        # Load camera configs from vendor
+        vendor/lib/libmmcamera2_sensor_modules.so)
+            sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "${2}"
+            ;;
+        # Drod unused dependency
+        vendor/lib/libmmcamera_vstab_module.so)
+            "${PATCHELF}" --remove-needed libandroid.so "${2}"
             ;;
         # Fix missing symbols
-        vendor/lib64/libril-qc-hal-qmi.so)
-            for  LIBCUTILS_SHIM in $(grep -L "libcutils_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libcutils_shim.so" "$LIBCUTILS_SHIM"
+        vendor/lib/libmot_gpu_mapper.so)
+            for LIBGUI_SHIM in $(grep -L "libgui_shim_vendor.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libgui_shim_vendor.so" "${LIBGUI_SHIM}"
             done
+            ;;
+        # Load camera metadata shim
+        vendor/lib/hw/camera.msm8998.so)
+            "${PATCHELF}" --replace-needed libcamera_client.so libcamera_metadata_helper.so "${2}"
+            ;;
+        # Correct mods gid
+        etc/permissions/com.motorola.mod.xml)
+            sed -i "s|vendor_mod|oem_5020|g" "${2}"
+            ;;
+        # Add uhid group for fingerprint service
+        vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc)
+            sed -i "s/system input/system uhid input/" "${2}"
+            ;;
+        # Patch libcutils dep into audio HAL
+        vendor/lib/hw/audio.primary.msm8998.so)
+            "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
             ;;
     esac
 }
