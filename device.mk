7517a95fd3d73da8e73f803580eea8fdff7c38c8
Georg Veichtlbauer <georg@vware.at>
2023-04-17 18:27:29 +0200

    nash: overlay: Split out Lineage settings
    Change-Id: I1ac3838375e6d1b2e13c3fcaeea684c76b417135

--- device.mk
+++ device.mk
@@ -15,7 +15,10 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # A/B updater
