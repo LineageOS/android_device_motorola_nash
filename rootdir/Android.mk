463d979ecf26896a7055460d54a779492266d166
Georg Veichtlbauer <georg@vware.at>
2023-05-14 20:42:12 +0200

    nash: Commonize init.gbmods.sh
    Change-Id: Ifd08aef4a4569af0cb63b4352e9f93ddd9dff503

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -16,11 +16,3 @@ LOCAL_SRC_FILES    := etc/init.nash.rc
 LOCAL_VENDOR_MODULE    := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.gbmods.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := bin/init.gbmods.sh
-LOCAL_VENDOR_MODULE    := true
-include $(BUILD_PREBUILT)
