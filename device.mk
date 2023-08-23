74418e5f6ec510586d09b635d2d18304b05768e2
Nolen Johnson <johnsonnolen@gmail.com>
2023-08-23 01:15:45 -0400

    nash: Properly exclude AudioFX
    * Lineage changed this to use product makefiles, not Android.mk's.
    
    Refernce: https://review.lineageos.org/q/topic:%22eleven-audiofx-exclude%22
    Change-Id: Ia0103ec2fde2ed5ad6958cd670f05df3a330ce7d

--- device.mk
+++ device.mk
@@ -36,6 +36,8 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
 # Audio
+TARGET_EXCLUDES_AUDIOFX := true
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
