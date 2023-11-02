f612278eb08bb82f15cca97bd106a3955f0828e3
Georg Veichtlbauer <georg@vware.at>
2023-11-02 22:49:37 +0100

    nash: Include fstab with OEM vendor partition
    Change-Id: Id9e99802aaea7c18127bbb9c7f6be6421cdce1a5

--- device.mk
+++ device.mk
@@ -91,6 +91,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hdr_tm_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hdr_tm_config.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    fstab.qcom.oem
+
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
