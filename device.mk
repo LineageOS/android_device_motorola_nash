2758811f26d640ce47930fb9676047ae7be3bd48
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-27 13:59:58 -0500

    nash: Reclassify Touch HAL
    Change-Id: I63267eba6f74c7ed26a4f71e2da945021782e543

--- device.mk
+++ device.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
-# Lineage hardware
+# Lineage Touch HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.msm8998
 
