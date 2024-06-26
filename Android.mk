feb8622e4d3b0b4c383f3f4d28fe6c6c27206f6e
Georg Veichtlbauer <georg@vware.at>
2024-06-26 09:40:11 +0200

    nash: Declare MotoMods libs as symlinks during extraction
    Change-Id: Id857d9057d0ba9436d86e2616775fa1b31c5d6aa

--- Android.mk
+++ Android.mk
@@ -28,14 +28,4 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter nash, $(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
-MODS_LIBS := libmodhw.so libvibratorhw.so
-MODS_SYMLINKS := $(addprefix $(TARGET_OUT)/priv-app/ModFmwkProxyService/lib/arm64/,$(notdir $(MODS_LIBS)))
-$(MODS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "MODS lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /system/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(MODS_SYMLINKS)
-
 endif
