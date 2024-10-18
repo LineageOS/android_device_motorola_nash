3e5fa7953a325f55c4e5597fd45aad69951d6434
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    nash: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_nash.mk
+++ lineage_nash.mk
@@ -7,7 +7,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/nash/device.mk)
 
 # Device identifiers
-BUILD_FINGERPRINT := motorola/nash_retail/nash:9/PPX29.159-24/e78f1:user/release-keys
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := nash
 PRODUCT_MANUFACTURER := motorola
@@ -15,5 +14,6 @@ PRODUCT_MODEL := Moto Z (2)
 PRODUCT_NAME := lineage_nash
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-     PRODUCT_NAME=nash \
-     PRIVATE_BUILD_DESC="nash-user 9 PPX29.159-24 e78f1 release-keys"
+    BuildDesc="nash-user 9 PPX29.159-24 e78f1 release-keys" \
+    BuildFingerprint=motorola/nash_retail/nash:9/PPX29.159-24/e78f1:user/release-keys \
+    DeviceName=nash
