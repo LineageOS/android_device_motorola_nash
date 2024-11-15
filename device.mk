40f2246a84bf933b801d02e74b05cc64f1831335
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-15 00:27:48 -0500

    nash: Commonize libhidlbase-v32
    Change-Id: Ic6757897e52b7783ee2f58ff84edee4ec842ced1

--- device.mk
+++ device.mk
@@ -157,11 +157,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
-# VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libhidlbase-v32.so \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
-
 # VR
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \
