9a0843bd77dd3335872c93c90edf91e994bec5ad
Nolen Johnson <johnsonnolen@gmail.com>
2025-02-21 12:31:56 -0500

    nash: TARGET_USES_OEM_AS_VENDOR isn't a boardflag anymore
    Change-Id: Idca52258dd856296861db7ca3d6d9c27e1ea9d7f

--- device.mk
+++ device.mk
@@ -157,4 +157,5 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Inherit from motorola msm8998-common
 TARGET_MOTO_PLATFORM := msm8998
+TARGET_USES_OEM_AS_VENDOR := true
 $(call inherit-product, device/motorola/msm8998-common/common.mk)
