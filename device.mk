79aac7a49bc7490e0d099bfc51b23111825b5735
Nolen Johnson <johnsonnolen@gmail.com>
2025-07-31 13:20:11 -0400

    nash: Build ModsManifest app
    Change-Id: I50651fa627281b249902f96dece82a8f7c85a7d4

--- device.mk
+++ device.mk
@@ -120,6 +120,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Permissions
+PRODUCT_PACKAGES += \
+    ModsManifest
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
